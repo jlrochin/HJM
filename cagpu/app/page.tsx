1cc2d55c0278d03bc65e1a269267798cc5181372
jlrochin <jlrochin@users.noreply.github.com>
2025-09-08 13:54:30 -0600

    Actualizar la redirección en la página principal de CAGPU para dirigir a '/login' en lugar de '/cagpu/login'.

--- cagpu/app/page.tsx
+++ cagpu/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation'
 
 export default function Page() {
   // Redirección server-side para evitar cualquier render en /cagpu
-  redirect('/cagpu/login')
+  redirect('/login')
 }
